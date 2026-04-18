69453d609d6efe5306fa6995eb21ad8dfa99b1e8
292akhil2929-cmyk <murali.krishna.ssa@gmail.com>
2026-04-18 19:28:18 +0400

    Allow all image domains for product images

--- next.config.ts
+++ next.config.ts
@@ -17,14 +17,8 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pg'],
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "*.supabase.co",
-        pathname: "/storage/v1/object/public/**",
-      },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "*.amazon.ae" },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
   experimental: {
